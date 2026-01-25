0264f80aa0afbdd30c71c63fe6ae9b31febce1da
Ritik <ritik.kumar16@yahoo.com>
2026-01-25 23:08:18 +0530

    changes in backend db url

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,12 +1,10 @@
 const { Pool } = require("pg");
-const env = require("./env");
 
 const pool = new Pool({
-  host: env.DB_HOST,
-  port: Number(env.DB_PORT),
-  user: env.DB_USER,
-  password: env.DB_PASSWORD,
-  database: env.DB_NAME
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
